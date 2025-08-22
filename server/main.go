660df7764c007af0a84c2a3b1c67108a8a1c1a64
BasileBux <basile.buxtorf@ik.me>
2025-08-22 17:30:41 +0200

    testing CI/CD for deploy with stuff fixed

--- server/main.go
+++ server/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, New World!")
+		fmt.Fprintf(w, "Hello, World!")
 	})
 	http.ListenAndServe(":8080", nil)
 }
