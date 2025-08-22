f22dda75a28135f1d0db8a86122bbb6a83cd92b9
BasileBux <basile.buxtorf@ik.me>
2025-08-22 16:20:21 +0200

    working docker go server

--- server/setup.sh
+++ server/setup.sh
@@ -1,19 +1,14 @@
 #/usr/bin/env bash
 
-# Setup on Hetzner Cloud
-
-server_ip="your_server_ip"
+# Run this script on the server to set up everything
 
 sudo apt update
 sudo apt install -y docker.io
 sudo systemctl start docker
 sudo systemctl enable docker
 
-sudo add-apt-repository -y ppa:longsleep/golang-backports
-sudo apt update
-sudo apt install -y golang-go
-
-mkdir server
-
 sudo usermod -aG docker $USER
 logout
+
+docker pull vasilba/homeal:latest
+docker run -d -p 8080:8080 vasilba/homeal:latest
