0e546283dbee2028543346e0ac0df99f9ad21f44
BasileBux <basile.buxtorf@ik.me>
2025-08-25 08:58:29 +0200

    go server listening on 2 ports for website and API

--- server/main.go
+++ server/main.go
@@ -1,11 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func main() {
-	fs := http.FileServer(http.Dir("./landing-page"))
-	http.Handle("/", fs)
-	http.ListenAndServe(":3000", nil)
+	go func() {
+		mux1 := http.NewServeMux()
+		fs := http.FileServer(http.Dir("./landing-page"))
+		mux1.Handle("/", fs)
+		if err := http.ListenAndServe(":80", mux1); err != nil {
+			fmt.Printf("Server landing-page error: %v\n", err)
+		}
+	}()
+
+	mux2 := http.NewServeMux()
+	mux2.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World!")
+	})
+	if err := http.ListenAndServe(":3000", mux2); err != nil {
+		fmt.Printf("Server API error: %v\n", err)
+	}
+
 }
